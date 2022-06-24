c9e241f70a605f7194af8674e604721cd1bfacfe
Joshua Maffuccio <joshuamaffuccio@Joshuas-MacBook-Air-2.local>
2022-06-24 12:13:23 -0700

    Connect to external site

--- functions/movies.js
+++ functions/movies.js
@@ -1,30 +1,47 @@
-const { URL } = require('url')
-const fetch = require('node-fetch')
-const movies = require('../data/movies.json')
+const { URL } = require('url');
+const fetch = require('node-fetch');
+const { query } = require('./util/hasura');
 
 exports.handler = async () => {
-  const api = new URL('https://www.omdbapi.com/')
-  api.searchParams.set('apiKey', process.env.OMDB_API_KEY)
+  const { movies } = await query({
+    query: `
+      query {
+        movies {
+          id
+          poster
+          tagline
+          title
+        }
+      }
+    `,
+  });
+
+  const api = new URL('https://www.omdbapi.com/');
+
+  // add the secret API key to the query string
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY);
 
   const promises = movies.map((movie) => {
-    api.searchParams.set('i', movie.id)
+    // use the movie’s IMDb ID to look up details
+    api.searchParams.set('i', movie.id);
 
     return fetch(api)
       .then((response) => response.json())
       .then((data) => {
-        const scores = data.Ratings
+        const scores = data.Ratings;
 
         return {
           ...movie,
           scores,
-        }
-      })
-  })
+        };
+      });
+  });
 
-  const moviesWithRatings = await Promise.all(promises)
+  // https://lwj.dev/blog/keep-async-await-from-blocking-execution/
+  const moviesWithRatings = await Promise.all(promises);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(moviesWithRatings)
-  }
-}
+    body: JSON.stringify(moviesWithRatings),
+  };
+};
