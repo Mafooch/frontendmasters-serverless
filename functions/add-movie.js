5b53aa16e256c8c030186cfc9d40ae7783d8849c
Joshua Maffuccio <joshuamaffuccio@Joshuas-MacBook-Air-2.local>
2022-06-27 10:36:12 -0700

    Add role-based auth

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,8 +1,18 @@
 const { query } = require('./utils/hasura')
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const { id, title, tagline, poster } = JSON.parse(event.body)
-  console.log(event)
+  const { user } = context.clientContext
+  const isLoggedIn = user && user.app_metadata
+  const roles = user.app_metadata.roles || []
+  console.log('roles: ', roles)
+
+  if(!isLoggedIn || !roles.includes('admin')) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized'
+    }
+  }
   
   const result = await query({
     query: `
